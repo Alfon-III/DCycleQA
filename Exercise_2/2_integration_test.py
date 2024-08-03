d908e24650f24a261215c1f340765bf8d3085a00
Alfon-III <alfonsotercer@gmail.com>
2024-08-03 13:45:36 +0200

    fixed and added test and excel

--- Exercise_2/2_integration_test.py
+++ Exercise_2/2_integration_test.py
@@ -1,7 +1,16 @@
 from CarbonFootprintCalculator import get_user_carbon_footprint, get_comparison_with_average
 import pytest
 
-# python3 -m pytest -v ./Exercise\ 2/2_integration_test.py
+
+
+# Excel to test the results
+# https://docs.google.com/spreadsheets/d/1uDtg-gmnTwtIeVu7pdKuRatCRWwqIjlosS34Xr1C99U/edit?usp=sharing
+
+# Run integration Tests:
+# Run tests:
+# pytest .\Exercise_2\2_integration_test.py
+
+
 
 def test_get_user_carbon_footprint():
     user_data = {
@@ -44,9 +53,66 @@ def test_comparison_with_avg_fp():
 
     user_footprint = get_user_carbon_footprint(user_data)
     average_footprint = get_user_carbon_footprint(avg_fp)
-    
+
     assert expected_res == get_comparison_with_average(user_footprint, average_footprint)
-     
+
+
+def test_comparison_with_avg_fp_2():
+    
+    user_data = {
+        'energy_consumption': 80,
+        'transportation': 20.5,
+        'waste': -50
+    }
+
+    avg_fp = {
+        'energy_consumption': 60,
+        'transportation': 20,
+        'waste': 10
+    }
+    
+    expected_res = {
+        'energy_difference': 4.66,
+         'transportation_difference': 0.105,
+         'waste_difference': -3.000,
+         'total_difference': 1.765
+    }
+
+    user_footprint = get_user_carbon_footprint(user_data)
+    average_footprint = get_user_carbon_footprint(avg_fp)
+
+    result = get_comparison_with_average(user_footprint, average_footprint)
+
+    assert result == pytest.approx(expected_res, rel=1e-9)
+
+def test_comparison_with_avg_fp_decimals():
+    
+    user_data = {
+        'energy_consumption': 123.456,
+        'transportation': 8.88888,
+        'waste': .000555
+    }
+
+    avg_fp = {
+        'energy_consumption': 985.25489,
+        'transportation': 0.5298,
+        'waste': 0.0321654
+    }
+    
+    expected_res = {
+        'energy_difference': -200.79914137,
+         'transportation_difference': 1.7554067999999998,
+         'waste_difference': -0.00158052,
+         'total_difference': -199.04531509
+    }
+
+    user_footprint = get_user_carbon_footprint(user_data)
+    average_footprint = get_user_carbon_footprint(avg_fp)
+
+    assert expected_res == get_comparison_with_average(user_footprint, average_footprint)
+
+
+
 def test_comparison_with_avg_fp_case_equal():
     user_data = {
         'energy_consumption': 100,
@@ -75,4 +141,4 @@ def test_comparison_with_avg_fp_case_equal():
     assert result == pytest.approx(expected_res, rel=1e-9)
        
      
-    
+
